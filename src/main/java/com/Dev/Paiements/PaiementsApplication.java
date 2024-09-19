88ce104aeeeb831b79499409c59b401b351faf99
AMG_TECH_SEVVY <amdy2.gueye@ucad.edu.sn>
2024-09-19 20:18:31 +0200

    first commit

---
+++ src/main/java/com/Dev/Paiements/PaiementsApplication.java
@@ -0,0 +1,13 @@
+package com.Dev.Paiements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaiementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaiementsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Dev/Paiements/PaiementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Dev.Paiements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaiementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
